6445de55236c0622e3f5be7e89b53545a15a1c98
alpaypasali <alpaypasali@gmail.com>
2024-05-09 20:24:38 +0300

    Add project files.

---
+++ E-CommerceWithIdentity/Constants/Key.cs
@@ -0,0 +1,8 @@
+﻿namespace E_CommerceWithIdentity.Constants
+{
+    public static class Key
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
+    }
+}
---
+++ E-CommerceWithIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_CommerceWithIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

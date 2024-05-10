f74466921af3a822b66cda42658a3e40c48859f2
alpaypasali <alpaypasali@gmail.com>
2024-05-10 19:04:14 +0300

    fix

---
+++ E-CommerceWithIdentity/Controllers/ProductController.cs
@@ -0,0 +1,72 @@
+﻿using E_Commerce_Shared.Dto;
+using E_Commerce_Shared.Entity;
+using E_CommerceWithIdentity.Models;
+using E_CommerceWithIdentity.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace E_CommerceWithIdentity.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(IProductService productService, ICategoryService categoryService, IWebHostEnvironment webHostEnvironment)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            ProductModel model = new ProductModel();
+            LoadCategorySelectDataView();
+            ViewData["Title"] = "Ürün oluştur";
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                ProductDto productDto = new ProductDto();
+                productDto.ProductDescription = model.ProductDescription;
+                productDto.ProductName = model.ProductName;
+                productDto.Price = model.Price;
+                productDto.CategoryId = model.CategoryId;   
+                if(model.CoverPhoto is not null)
+                {
+                    string folder = "Images\\";
+                    folder += model.CoverPhoto.FileName;
+                    string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folder);
+                    await model.CoverPhoto.CopyToAsync(new FileStream(serverFolder, FileMode.Create));  
+
+                }
+                productDto.ImageUrl = model.CoverPhoto.FileName;
+                var result = await _productService.CreateProduct(productDto);
+                if(result.Success is true)
+                {
+                    return RedirectToAction("CreateProduct", "Product");
+                }
+
+            }
+            return RedirectToAction("CreateProduct", "Product");
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private void LoadCategorySelectDataView()
+        {
+            List<Category> categories = _categoryService.GetCategories();
+            List<SelectListItem> selectListItems = categories.Select(x=> new SelectListItem(x.CategoryName,x.Id.ToString())).ToList();
+            ViewData["categories"] = selectListItems;
+        }
+    }
+}
---
+++ E-CommerceWithIdentity/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceWithIdentity.Models
+{
+    public class ProductModel
+    {
+        public string? ProductName { get;set; }
+        [MaxLength(60)]
+        public string ProductDescription { get;set; }
+        public decimal Price { get;set; }
+        public string? ImageUrl { get;set; }
+        public IFormFile CoverPhoto { get;set; }    
+        public int CategoryId { get;set; }
+    }
+}
